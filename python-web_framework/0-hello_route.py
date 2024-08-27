557bfcdf2f52797207eabea3fa6ba79edf839873
evisp <evisplaku@gmail.com>
2024-08-27 10:13:23 +0200

    setup flask project

--- python-web_framework/0-hello_route.py
+++ python-web_framework/0-hello_route.py
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 @app.route("/", strict_slashes=False)
 def hello():
-    """Function that returns a simple string"""
+    """Returns a simple string"""
     return "Hello HBNB!"
 
 
