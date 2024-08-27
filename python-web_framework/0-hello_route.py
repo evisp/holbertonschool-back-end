9c80cb59afb39d828afe3479499aa76ce8f8f0b3
evisp <evisplaku@gmail.com>
2024-08-27 10:14:05 +0200

    setup flask project

--- python-web_framework/0-hello_route.py
+++ python-web_framework/0-hello_route.py
@@ -1,16 +1,16 @@
 #!/usr/bin/python3
-""" A script that starts a Flask web application"""
+"""Importing the needed framework"""
 from flask import Flask
 
 
 app = Flask(__name__)
 
 
-@app.route("/", strict_slashes=False)
+@app.route("/airbnb-onepage/", strict_slashes=False)
 def hello():
-    """Returns a simple string"""
+    """Function that returns a simple string"""
     return "Hello HBNB!"
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5000)
